c850247c3a34d3ebd3df51ad2832d88e81e9ee87
Slav Lazarevic <slav.lazarevic@ibm.com>
2025-07-03 23:35:55 +1000

    chore: vite and tsconfig updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +16,21 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@i18n": path.resolve(__dirname, "src/i18n"),
+      "@layout": path.resolve(__dirname, "src/layout"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 });
