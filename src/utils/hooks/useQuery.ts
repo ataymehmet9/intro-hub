05fb9af45cadadd3394eccf4bf3dbab325940985
slavlazar <slavlazar@gmail.com>
2026-02-23 23:59:16 +1100

    fix: deleted shit file

--- src/utils/hooks/useQuery.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react'
-import { useLocation } from '@tanstack/react-router'
-
-export default function useQuery(): URLSearchParams {
-    const { search } = useLocation()
-    return useMemo(() => new URLSearchParams(search), [search])
-}
